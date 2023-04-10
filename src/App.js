db8256a08512343e33366adcf378dee57b49f2ff
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-04-10 15:11:01 -0700

    created starter react files and stylesheets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import React from 'react';
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatAzulesCoffee,
+  Header,
+} from './containers';
+
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
 function App() {
-  return <>Hello World!</>;
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatAzulesCoffee />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/containers/whatAzulesCoffee/WhatAzulesCoffee.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './whatazulescoffee.css';
+
+const WhatAzulesCoffee = () => {
+  return <div>WhatAzulesCoffee</div>;
+};
+
+export default WhatAzulesCoffee;
